7a0c34f3404443625b1e5131cef2298bc6ad99f8
Danial <admin@redsparr0w.com>
2021-11-11 14:08:20 +1300

    Initial working example

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+// Required for login
+// Get these from https://discord.com/developers/applications/ → OAuth2
+define('OAUTH2_CLIENT_ID', 'DISCORD_BOT_CLIENT_ID');
+define('OAUTH2_CLIENT_SECRET', 'DISCORD_BOT_CLIENT_SECRET');
+define('OAUTH2_REDIRECT_URI', 'YOUR_WEBSITE_ADDRESS');
+
+// Show discord server online count and invite link button
+// Comment these out if not needed
+define('DISCORD_SERVER_ID', 'YOUR_DISCORD_SERVER_ID');
+define('DISCORD_SERVER_INVITE', 'YOUR_DISCORD_SERVER_INVITE_LINK');
