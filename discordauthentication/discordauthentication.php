6d092aee20df8aa01f98a238aed72c6dc2d7ae03
Danial <admin@redsparr0w.com>
2021-11-11 15:12:10 +1300

    separate page and oauth

---
+++ discordauthentication/discordauthentication.php
@@ -0,0 +1,120 @@
+<?php
+require('./config.php');
+
+$authorizeURL = 'https://discord.com/api/oauth2/authorize';
+$tokenURL = 'https://discord.com/api/oauth2/token';
+$apiURLBase = 'https://discord.com/api/users/@me';
+$revokeURL = 'https://discordapp.com/api/oauth2/token/revoke';
+
+if (get('return_uri')) {
+    $_SESSION['return_uri'] = get('return_uri');
+}
+
+// Start the login process by sending the user to Discord's authorization page
+if(get('action') == 'login') {
+    session_start();
+
+    $params = array(
+        'client_id' => OAUTH2_CLIENT_ID,
+        'redirect_uri' => OAUTH2_REDIRECT_URI,
+        'response_type' => 'code',
+        'scope' => 'identify'
+    );
+    
+    // Redirect the user to Discord's authorization page
+    header('Location: https://discordapp.com/api/oauth2/authorize?' . http_build_query($params));
+    die();
+}
+
+if(get('action') == 'logout') {
+    apiRequest($revokeURL, array(
+        'token' => session('discord_access_token'),
+        'client_id' => OAUTH2_CLIENT_ID,
+        'client_secret' => OAUTH2_CLIENT_SECRET,
+    ));
+      
+    // Log user out of our site
+    unset($_SESSION['discord_access_token']);
+    unset($_SESSION['discord_id']);
+    unset($_SESSION['discord_avatar']);
+    unset($_SESSION['discord_username']);
+    unset($_SESSION['discord_discriminator']);
+    unset($_SESSION['discord_public_flags']);
+    
+    // Go back to homepage
+    header('Location: /');
+    die();
+}
+
+// When Discord redirects the user back here, there will be a "code" and "state" parameter in the query string
+if(get('code')) {
+    // Exchange the auth code for a token
+    $token = apiRequest($tokenURL, array(
+        'grant_type' => 'authorization_code',
+        'client_id' => OAUTH2_CLIENT_ID,
+        'client_secret' => OAUTH2_CLIENT_SECRET,
+        'redirect_uri' => OAUTH2_REDIRECT_URI,
+        'code' => get('code')
+    ));
+    
+    if ($token->error) {
+        // Dump the token data if debugging
+        // var_dump($token);
+        die('Something went wrong, try again later..');
+    }
+  
+    $_SESSION['discord_access_token'] = $token->access_token;
+    
+    $user = apiRequest($apiURLBase);
+  
+    // Dump user data if debugging
+    // var_dump($user); die();
+    
+    $_SESSION['discord_id'] = $user->id;
+    $_SESSION['discord_avatar'] = $user->avatar ?? '';
+    $_SESSION['discord_username'] = $user->username;
+    $_SESSION['discord_discriminator'] = $user->discriminator;
+    $_SESSION['discord_public_flags'] = $user->public_flags ?? 0;
+
+    header('Location: ' . strtok($_SERVER['REQUEST_URI'], '?'));
+}
+
+function apiRequest($url, $post=FALSE, $headers=array()) {
+    $ch = curl_init($url);
+    curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
+
+    $response = curl_exec($ch);
+
+
+    if($post)
+        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
+
+    $headers[] = 'Accept: application/json';
+
+    if(session('discord_access_token'))
+        $headers[] = 'Authorization: Bearer ' . session('discord_access_token');
+
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    $response = curl_exec($ch);
+    return json_decode($response);
+}
+
+function get($key, $default=NULL) {
+    return array_key_exists($key, $_GET) ? $_GET[$key] : $default;
+}
+
+function session($key, $default=NULL) {
+    return array_key_exists($key, $_SESSION) ? $_SESSION[$key] : $default;
+}
+
+// If user logged in
+if(session('discord_id')) {
+    // If return uri is set
+    if (session('return_uri')) {
+        header('Location: ' . session('return_uri'));
+        unset($_SESSION['return_uri']);
+        die('Redirecting to ' . session('return_uri'));
+    }
+}
--- index.php
+++ index.php
@@ -5,112 +5,7 @@ error_reporting(0);ini_set('display_errors', 0);
 ini_set('max_execution_time', 30);
 
 include('../session.php');
-require('./config.php');
-
-$authorizeURL = 'https://discord.com/api/oauth2/authorize';
-$tokenURL = 'https://discord.com/api/oauth2/token';
-$apiURLBase = 'https://discord.com/api/users/@me';
-$revokeURL = 'https://discordapp.com/api/oauth2/token/revoke';
-
-// Start the login process by sending the user to Discord's authorization page
-if(get('action') == 'login') {
-    session_start();
-
-    $params = array(
-        'client_id' => OAUTH2_CLIENT_ID,
-        'redirect_uri' => OAUTH2_REDIRECT_URI,
-        'response_type' => 'code',
-        'scope' => 'identify'
-    );
-    
-    // Redirect the user to Discord's authorization page
-    header('Location: https://discordapp.com/api/oauth2/authorize?' . http_build_query($params));
-    die();
-}
-
-if(get('action') == 'logout') {
-    apiRequest($revokeURL, array(
-        'token' => session('discord_access_token'),
-        'client_id' => OAUTH2_CLIENT_ID,
-        'client_secret' => OAUTH2_CLIENT_SECRET,
-    ));
-      
-    // Log user out of our site
-    unset($_SESSION['discord_access_token']);
-    unset($_SESSION['discord_id']);
-    unset($_SESSION['discord_avatar']);
-    unset($_SESSION['discord_username']);
-    unset($_SESSION['discord_discriminator']);
-    unset($_SESSION['discord_public_flags']);
-    
-    // Reload page
-    header('Location: ' . strtok($_SERVER["REQUEST_URI"], '?'));
-    die();
-}
-
-// When Discord redirects the user back here, there will be a "code" and "state" parameter in the query string
-if(get('code')) {
-    // Exchange the auth code for a token
-    $token = apiRequest($tokenURL, array(
-        'grant_type' => 'authorization_code',
-        'client_id' => OAUTH2_CLIENT_ID,
-        'client_secret' => OAUTH2_CLIENT_SECRET,
-        'redirect_uri' => OAUTH2_REDIRECT_URI,
-        'code' => get('code')
-    ));
-    
-    if ($token->error) {
-        // Dump the token data if debugging
-        // var_dump($token);
-        die('Something went wrong, try again later..');
-    }
-  
-    $_SESSION['discord_access_token'] = $token->access_token;
-    
-    $user = apiRequest($apiURLBase);
-  
-  	// Dump user data if debugging
-  	// var_dump($user); die();
-    
-    $_SESSION['discord_id'] = $user->id;
-    $_SESSION['discord_avatar'] = $user->avatar ?? '';
-    $_SESSION['discord_username'] = $user->username;
-    $_SESSION['discord_discriminator'] = $user->discriminator;
-    $_SESSION['discord_public_flags'] = $user->public_flags ?? 0;
-
-    header('Location: ' . strtok($_SERVER['REQUEST_URI'], '?'));
-}
-
-function apiRequest($url, $post=FALSE, $headers=array()) {
-    $ch = curl_init($url);
-    curl_setopt($ch, CURLOPT_IPRESOLVE, CURL_IPRESOLVE_V4);
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, TRUE);
-
-    $response = curl_exec($ch);
-
-
-    if($post)
-        curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($post));
-
-    $headers[] = 'Accept: application/json';
-
-    if(session('discord_access_token'))
-        $headers[] = 'Authorization: Bearer ' . session('discord_access_token');
-
-    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-
-    $response = curl_exec($ch);
-    return json_decode($response);
-}
-
-function get($key, $default=NULL) {
-    return array_key_exists($key, $_GET) ? $_GET[$key] : $default;
-}
-
-function session($key, $default=NULL) {
-    return array_key_exists($key, $_SESSION) ? $_SESSION[$key] : $default;
-}
-
+require('./discordauthentication/discordauthentication.php');
 ?>
 <head>
     <meta charset="utf-8">
@@ -126,25 +21,25 @@ function session($key, $default=NULL) {
 if(session('discord_id')) {
     // User is logged in
     ?>
-    <img class="mb-4 rounded-circle" src="https://cdn.discordapp.com/avatars/<?=$_SESSION['discord_id']?>/<?=$_SESSION['discord_avatar']?>.png?size=256" alt="" width="72" height="72" onerror="this.src = 'assets/images/Discord_icon.svg'">
-    <h1 class="h3 mb-3 font-weight-normal"><?=$_SESSION['discord_username']?>#<?=$_SESSION['discord_discriminator']?></h1>
-    <p class="text-muted"><small><?=$_SESSION['discord_id']?></small></p>
-	<p>
-  	<?php
-    // Show the users profile badges
-    for ($i = 0; $i < 20; $i++){
-        if ($_SESSION['discord_public_flags'] & (1 << $i))
-            echo '<img height="16px" width="16px" src="assets/images/badges/' . $i . '.png" onerror="this.remove();"/>';
-    }
-    ?>
-  	</p>
+    <img class="mb-4 rounded-circle" src="https://cdn.discordapp.com/avatars/<?=session('discord_id')?>/<?=session('discord_avatar')?>.png?size=256" alt="" width="72" height="72" onerror="this.src = 'assets/images/Discord_icon.svg'">
+    <h1 class="h3 mb-3 font-weight-normal"><?=session('discord_username')?>#<?=session('discord_discriminator')?></h1>
+    <p class="text-muted"><small><?=session('discord_id')?></small></p>
+    <p>
+        <?php
+        // Show the users profile badges
+        for ($i = 0; $i < 20; $i++){
+            if (session('discord_public_flags') & (1 << $i))
+                echo '<img height="16px" width="16px" src="assets/images/badges/' . $i . '.png" onerror="this.remove();"/>';
+        }
+        ?>
+    </p>
     <a class="btn btn-lg btn-danger btn-block" href="?action=logout">LOG OUT</a>
     <?php
 } else {
     // User is not logged in
     ?>
     <img class="mb-4" src="assets/images/Discord_logo.svg" alt="" width="100%">
-    <a class="btn btn-lg btn-discord btn-block" href="?action=login">LOG IN</a>
+    <a class="btn btn-lg btn-discord btn-block" href="?action=login&return_uri=<?=$_SERVER['REQUEST_URI']?>">LOG IN</a>
     <?php
 }
 
