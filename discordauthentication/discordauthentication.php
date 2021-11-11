5391f24c8bff756fd079a1673f217d1c56bcf55c
Danial <admin@redsparr0w.com>
2021-11-11 21:02:21 +1300

    Fixup

--- discordauthentication/discordauthentication.php
+++ discordauthentication/discordauthentication.php
@@ -1,5 +1,5 @@
 <?php
-require('./config.php');
+require(__DIR__ . '/config.php');
 
 $authorizeURL = 'https://discord.com/api/oauth2/authorize';
 $tokenURL = 'https://discord.com/api/oauth2/token';
